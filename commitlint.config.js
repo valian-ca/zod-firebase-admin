b0975457d8b3f2cbc393ddbbd7b9dd5d6b608753
Julien Marcil <julien@valian.ca>
2023-05-26 16:07:40 -0400

    chore: project created

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+const ERROR = 2
+const ALWAYS = 'always'
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  defaultIgnores: true,
+  parserPreset: {
+    parserOpts: {
+      noteKeywords: ['See', 'Signed-off-by', 'Co-authored-by'],
+    },
+  },
+  rules: {
+    'footer-leading-blank': [ERROR, ALWAYS],
+    'footer-max-line-length': [ERROR, ALWAYS, Number.POSITIVE_INFINITY],
+  },
+}
