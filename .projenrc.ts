4a8407c967380309601f2121ff845f72dbf63cf0
Julien Marcil <julien@valian.ca>
2023-05-31 21:43:10 -0400

    project setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,78 @@
-import { typescript } from 'projen';
-const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'zod-firebase-admin',
-  projenrcTs: true,
+import { typescript } from 'projen'
+import { TypescriptConfigExtends } from 'projen/lib/javascript'
+import { NodePackageManager, NpmAccess } from 'projen/lib/javascript/node-package'
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-});
-project.synth();
+const project = new typescript.TypeScriptProject({
+  projenrcTs: true,
+  name: 'zod-firebase-admin',
+  packageName: 'zod-firebase-admin',
+  description: 'zod firebase-admin',
+  repository: 'git+https://github.com/valian-ca/zod-firebase-admin.git',
+
+  minNodeVersion: '18.0.0',
+  workflowNodeVersion: 'lts/hydrogen',
+
+  authorName: 'Valian',
+  authorUrl: 'https://valian.ca',
+  authorOrganization: true,
+
+  license: 'MIT',
+  licensed: true,
+  copyrightOwner: 'Valian',
+
+  keywords: ['eslint'],
+
+  gitignore: ['.idea'],
+
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
+  defaultReleaseBranch: 'main',
+  packageManager: NodePackageManager.NPM,
+
+  prettier: true,
+  prettierOptions: { settings: { semi: false, singleQuote: true, printWidth: 120 } },
+  eslint: true,
+  jest: true,
+
+  tsconfig: {
+    extends: TypescriptConfigExtends.fromPaths(['@tsconfig/node18/tsconfig.json']),
+    compilerOptions: {
+      alwaysStrict: undefined,
+      declaration: undefined,
+      esModuleInterop: undefined,
+      experimentalDecorators: undefined,
+      inlineSourceMap: undefined,
+      inlineSources: undefined,
+      lib: undefined,
+      module: undefined,
+      noEmitOnError: undefined,
+      noFallthroughCasesInSwitch: undefined,
+      noImplicitAny: undefined,
+      noImplicitReturns: undefined,
+      noImplicitThis: undefined,
+      noUnusedLocals: undefined,
+      noUnusedParameters: undefined,
+      resolveJsonModule: undefined,
+      strict: undefined,
+      strictNullChecks: undefined,
+      strictPropertyInitialization: undefined,
+      stripInternal: undefined,
+      target: undefined,
+    },
+  },
+
+  peerDeps: ['firebase-admin@>=11.8.0'],
+  deps: ['type-fest', 'zod'],
+
+  devDeps: [
+    '@valian/eslint-config',
+    '@commitlint/cli',
+    '@commitlint/config-conventional',
+    '@tsconfig/node18',
+    'husky',
+    'markdownlint-cli',
+    'prettier',
+  ],
+})
+
+project.synth()
