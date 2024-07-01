83f20cd24235f30160660ca2be656e319d898151
Julien Marcil <julien@valian.ca>
2024-07-01 14:50:15 -0400

    docs(root): add jsdoc

--- packages/zod-firebase/src/collection-builder.ts
+++ packages/zod-firebase/src/collection-builder.ts
@@ -1,5 +1,10 @@
 import { type Collections, collectionsFactory, type FirestoreZodFactoryOptions, type Schema } from './factories'
 
+/**
+ * Build collections from a schema
+ * @param schema
+ * @param options
+ */
 export const collectionsBuilder = <TSchema extends Schema>(
   schema: TSchema,
   options?: FirestoreZodFactoryOptions,
